6cfd55224cb080b79b2cd9208a830c93775049ce
Nam Pham <nam@tokamak.network>
2025-04-15 14:41:45 +0000

    chore: add version

--- commands/version.go
+++ commands/version.go
@@ -2,15 +2,28 @@ package commands
 
 import (
 	"context"
-	"fmt"
+	"runtime/debug"
 
-	"github.com/tokamak-network/trh-sdk/pkg/constants"
 	"github.com/urfave/cli/v3"
 )
 
 func ActionVersion() cli.ActionFunc {
 	return func(ctx context.Context, cmd *cli.Command) error {
-		fmt.Println(constants.VERSION)
+		version := GetVersionInfo()
+		if version == "" {
+			return cli.Exit("Version information not available", 1)
+		}
+		println("Version:", version)
 		return nil
 	}
+}
+
+func GetVersionInfo() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+
+	version := info.Main.Version // usually "vX.Y.Z" or commit hash
+	return version
 }
